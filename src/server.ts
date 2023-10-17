609758275bf49db756d2ed7cf275016b660c2c24
Osumah John Terry <osumahterry@gmail.com>
2023-10-17 18:33:27 +0100

    routers added

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,19 @@
-import * as express from "express"
-import * as config from "config"
-import * as bodyParser from "body-parser"
+import * as express from "express";
+import * as config from "config";
+import * as bodyParser from "body-parser";
 
-import connectDB from '../db/connect';
+import connectDB from "../db/connect";
 
-const app = express()
+const app = express();
 
-
-
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const start = async () => {
-        await connectDB(config.get("database.connection"))
-        app.listen(config.get("port"), () => console.log(`Listening on port ${config.get("port")}`))
-}
+  await connectDB(config.get("database.connection"));
+  app.listen(config.get("port"), () =>
+    console.log(`Listening on port ${config.get("port")}`)
+  );
+};
 
-
-start()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+start();
